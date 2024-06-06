d474e616ae395bed83411f6dd5668d4c9adee593
mr-vbharadwaj <varunbharadwajmv@gmail.com>
2024-06-06 21:04:41 +0530

    Initialization and some tweakings are done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { b_cloud01,
+  b_cloud02,
+  cloud1,
+  cloud2,
+  cloud3,
+  moon,
+  moonlight,
+  star } from './img/imports';
 
 function App() {
+
+  function myFunction(event) {
+    const x = event.target; 
+    x.classList.toggle("change");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href='./'>Mr.VB</a>
+        <div className="menuIcon" onClick={myFunction}>
+          <div className='bar1'></div>
+          <div className='bar2'></div>
+          <div className='bar3'></div>
+        </div>
       </header>
+      <div className='background'>
+        <img className='stars' src={star}></img>
+        <img className='light' src={moonlight} alt='MoonLight'></img>
+      </div>
+      <div className='clouds'>
+        <div className='cloud1'>
+          <img src={cloud1}></img>
+        </div>
+        <div className='cloud2'>
+          <img src={cloud2}></img>
+        </div>
+        <div className='cloud3'>
+          <img src={cloud3}></img>
+        </div>
+      </div>
+      <div className='banner'>
+        <h1>VARUN <br></br> BHARADWAJ</h1>
+      </div>
+      <div className='moon'>
+        <img src={moon}></img>
+      </div>
     </div>
   );
 }
---
+++ src/img/imports.js
@@ -0,0 +1,19 @@
+import b_cloud01 from './b_cloud01.png';
+import b_cloud02 from './b_cloud02.png';
+import cloud1 from './cloud1.png';
+import cloud2 from './cloud2.png';
+import cloud3 from './cloud3.png';
+import moon from './moon.png';
+import moonlight from './moonlight.png';
+import star from './star.png';
+
+export {
+    b_cloud01,
+    b_cloud02,
+    cloud1,
+    cloud2,
+    cloud3,
+    moon,
+    moonlight,
+    star
+}
